6ba718fcd5fe0e09a30c739441805b181c035d6b
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 11:32:23 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,16 @@
+package CoeJavaSession1;
+
+public class Assignment4 {
+	
+	public static void main(String args[]){
+		
+		int i;
+		
+		for(i=65;i<=90;i++){
+			
+		char c = (char)i;		
+			System.out.println("The equivalent ASCCI value of "+i+ " is : "+c);
+		}
+	}
+
+}
